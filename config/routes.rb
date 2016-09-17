a633dd6de77ccd7c02277681b1e27c8016e77357
Iliya Svetlichnii <batya2095@gmail.com>
2016-09-17 17:26:17 +0600

    add active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :guests
+  root "guests#index"
+   
 end
