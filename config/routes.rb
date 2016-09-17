e841a2f1ab5d9fc0a9e166cd2f771359a45cbf07
Daniiar <lakiwolf.90@mail.ru>
2016-09-17 19:45:39 +0600

    added layout for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :guests
-  root "guests#index"
+  devise_for :guests 
+  # do
+  # 	root "devise/registrations#new"
+  # end
    
 end
