1e279a6a5eac41594751e3f0e5dea900cbe4ecb4
toly KG <tolykg907@gmail.com>
2016-09-23 20:31:04 +0600

    4 Валидация на вход

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
+
+	belongs_to :position
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true, length: {maximum: 50}
   validates :skype, presence: true, length: {maximum: 50}
   validates :phone, presence: true, length: {maximum: 50}
 
-  belongs_to :position
+  
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: '123', password_confirmation: 'password')
 Position.create(title: "Студент")
 Position.create(title: "Преподаватель")
