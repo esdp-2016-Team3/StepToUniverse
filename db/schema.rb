f691e993a5c80d77c1038620623906ada204314c
Iliya Svetlichnii <batya2095@gmail.com>
2016-09-23 21:18:19 +0600

    Пофиксил баг с сидами, не совподали пороли

--- db/schema.rb
+++ db/schema.rb
@@ -43,45 +43,8 @@ ActiveRecord::Schema.define(version: 20160922140301) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "guests", force: :cascade do |t|
-    t.string   "name"
-    t.string   "phone"
-    t.string   "skype"
-    t.boolean  "role"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_guests_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_guests_on_reset_password_token", unique: true
-  end
-
   create_table "positions", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.string   "phone"
-    t.string   "skype"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "teachers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "phone"
-    t.string   "skype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
