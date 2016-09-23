41fc964c934d3892a2cf5d77cb0fbc1da9c4394f
Iliya Svetlichnii <batya2095@gmail.com>
2016-09-23 19:58:55 +0600

    #2 Создал в панели администратора разделы Студент и Преподаватель

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,19 +4,7 @@ ActiveAdmin.register User, as: 'Status' do
       User.where(status: 'notactive')
     end
   end
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-# 
+
   index do
     selectable_column
     column :name do |user|
@@ -36,3 +24,48 @@ ActiveAdmin.register User, as: 'Status' do
 
 
 end
+
+ActiveAdmin.register User, as: 'Student' do
+  controller do
+    def scoped_collection
+      User.where(status: 'active', position_id: 1)
+
+    end
+  end
+  
+  index do
+    selectable_column
+    column :name do |user|
+      link_to user.name, admin_admin_user_path(user)
+    end
+    column :email
+    column :phone
+    column :skype
+    actions
+  end
+
+
+end
+
+ActiveAdmin.register User, as: 'Teacher' do
+  controller do
+    def scoped_collection
+      User.where(status: 'active', position_id: 2)
+
+    end
+  end
+  
+  index do
+    selectable_column
+    column :name do |user|
+      link_to user.name, admin_admin_user_path(user)
+    end
+    column :email
+    column :phone
+    column :skype
+    actions
+  end
+
+
+end
+
