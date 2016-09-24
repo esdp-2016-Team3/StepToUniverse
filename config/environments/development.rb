ea0b4dcb1c4832ae81a8c510322626f7282eff1e
fedyay <danil.df.df@gmail.com>
2016-09-24 21:40:01 +0600

    #10 Добавил подтверждение пользователя по почте

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,30 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.assets.raise_runtime_errors = true
+
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.force_ssl = false
+
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :domain => 'gmail.com',
+      :authentication => :login,
+      :user_name => 'esdp20163@gmail.com',
+      :password => 'esdp2016',
+      :enable_starttls_auto => true,
+      :openssl_verify_mode => 'none'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
