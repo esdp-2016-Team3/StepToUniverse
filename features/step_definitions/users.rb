9727824d5fd1bfc20f27b6a971e70e664454ad7e
fedyay <danil.df.df@gmail.com>
2016-10-01 13:39:30 +0600

    #19 Дополнил тесты

--- features/step_definitions/users.rb
+++ features/step_definitions/users.rb
@@ -1,4 +1,4 @@
-constants = {s_email: 'stud1@gmail.com', s_password: 'asdasd', t_email: 'teach1@gmail.com', t_password: 'asdasd'}
+constants = {s_email: 'stud1@gmail.com', s_password: 'asdasd', t_email: 'teach1@gmail.com', t_password: 'asdasd', f_email: 'asd@asd.asd', f_password: 'asddsa'}
 When(/^я как неавторизованный нахожусь на главной странице$/) do
   visit('/')
   page.assert_text('контентная информация')
@@ -54,4 +54,17 @@ end
 
 When(/^деавторизация проходит успешно$/) do
   page.assert_text('контентная информация')
+end
+
+When(/^я пытаюсь авторизоватья как Незарегистрированный$/) do
+  visit('/users/sign_in')
+  within('#new_user') do
+    fill_in('Email', with: constants[:f_email])
+    fill_in('Пароль', with: constants[:f_password])
+  end
+  click_button('Войти')
+end
+
+When(/^авторизация не проходит$/) do
+  page.assert_text('Неверный адрес эл. почты или пароль.')
 end
