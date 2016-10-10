b5b4653bc757aa70e4e26ed5aa02c580897b83f3
Iliya Svetlichnii <batya2095@gmail.com>
2016-10-10 21:07:32 +0600

    #31 удалил js код, в связи с ненадобностью

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,17 +17,3 @@
 //= require jquery_nested_form
 //= require turbolinks
 //= require_tree .
-
-
-function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
-}
-
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g");
-  $(link).up().insert({
-        before: content.replace(regexp, new_id)
-  });
-}
