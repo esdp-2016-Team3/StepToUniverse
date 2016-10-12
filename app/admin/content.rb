97abdf68ab7649f003e9d89a5c53134cf614752e
Iliya Svetlichnii <batya2095@gmail.com>
2016-10-12 15:31:46 +0600

    #37 Добавил в панель администратора список литературы с функционалом CRUD

--- app/admin/content.rb
+++ app/admin/content.rb
@@ -20,3 +20,27 @@ permit_params :title, :description
 
 end
 
+ActiveAdmin.register Text_file do
+
+permit_params :title, :description
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :status
+      f.input :file, as: :file
+    end
+
+    f.submit
+  end
+
+  index do
+    selectable_column
+    column :name
+
+    actions
+  end
+
+end
+
