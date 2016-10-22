1c33fe27f8104f2ad7b20720bb4ffd2d9e773e46
Iliya Svetlichnii <batya2095@gmail.com>
2016-10-22 14:27:10 +0600

    #32 убрал лишнюю связь так как создал отдельную модель

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,6 @@
 class Question < ApplicationRecord
 	belongs_to :level
-	belongs_to :survey
-	has_many :answers, :dependent => :destroy
+	has_many :answers, dependent: :destroy
 	accepts_nested_attributes_for :answers, allow_destroy: true
-	validates :content, :presence => true
+	validates :content, presence: true
 end
