320180dd72d335225773e87c558f37b31ed07356
Iliya Svetlichnii <batya2095@gmail.com>
2016-10-22 14:24:13 +0600

    #45 внес соответсвующие изминения в связи переносом тестового задания в отдельную модель

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,4 @@
 class Survey < ApplicationRecord
-	has_many :questions, :dependent => :destroy
-	accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+	has_many :questions_for_students, dependent: :destroy
+	accepts_nested_attributes_for :questions_for_students, reject_if: lambda { |a| a[:title].blank? }, allow_destroy: true
 end
