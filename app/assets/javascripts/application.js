576652be490d20c505b7d5a32a79c8597151f7c5
Iliya Svetlichnii <batya2095@gmail.com>
2016-10-26 17:12:01 +0600

    #45 добавил функционал удаления и добавления вопросов и ответов

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,17 @@
 //= require_tree .
 
 
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
+
+function add_fields(link, association, content) {  
+    var new_id = new Date().getTime();  
+    var regexp = new RegExp("new_" + association, "g");  
+    $(link).parent().before(content.replace(regexp, new_id));  
+}
+
 (function() {
     setInterval(function() {
         var cnt = 0
