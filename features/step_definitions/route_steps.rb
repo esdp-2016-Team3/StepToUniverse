8e0702b5477eaaf988e28406e59d708cfcd21ddb
Maxim <karamba92@mail.ru>
2016-11-03 19:01:57 +0600

    #64 добавил новые строки и шаги к тестам

--- features/step_definitions/route_steps.rb
+++ features/step_definitions/route_steps.rb
@@ -1,29 +1,5 @@
-When(/^я на странице "\/admin\/login"$/) do
-  visit new_admin_user_session_path
-  sleep(0.5)
-end
-
-When(/^я на странице "\/users\/sign_in"$/) do
-  visit new_user_session_path
-  sleep(0.5)
-end
-
 When(/^должен перейти на главную страницу$/) do
   visit root_path
-  sleep(0.5)
-end
-
-When(/^я на странице "\/users\/sign_up"$/) do
-  visit new_user_registration_path
-  sleep(0.5)
-end
-
-When /^кликаю через xpath1 "([^"]*)" для "([^"]*)"$/ do |link, person|
-  # Use capybara to find row based on 'person' text... no need for the additional 'find'
-  # the '.,' sets the scope to the current node, that is the tr in question
-  within(:xpath, "//tbody/tr[@id='#{person}']") do
-    click_link(link)
-  end
   sleep(0.5)
 end
 
@@ -32,17 +8,17 @@ When(/^я на рутовой странице$/) do
   sleep(0.5)
 end
 
-When(/^перехожу на страницу почты$/) do
+When(/^перехожу на страницу почты Gmail$/) do
   visit('https://accounts.google.com')
-  sleep(3)
+  sleep(0.5)
 end
 
-When(/^перехожу на страницу visit\('https:\/\/mail\.google\.com\/mail\/'\)$/) do
+When(/^перехожу на страницу писем Николая Петровича$/) do
   visit('https://mail.google.com/mail/')
-  sleep(15)
+  sleep(0.5)
 end
 
-When(/^кликаю на письмо$/) do
-  find(:xpath, "//div[@class='Cp']/div/table/tbody/tr[1]/td[2]").click
-  sleep(20)
+When(/^кликаю на письмо через xpath$/) do
+  find(:xpath, "//*[@id=':2']/div/div[1]/div[7]").click
+  sleep(0.5)
 end
