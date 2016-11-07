5c5eb45ab9b394c5ee9305ac049e256559002bdb
Daniiar <lakiwolf.90@mail.ru>
2016-11-07 17:51:40 +0600

    #70 Добавил в ЛКП возможность отправки аудио файла

--- app/models/homework_result.rb
+++ app/models/homework_result.rb
@@ -1,5 +1,16 @@
 class HomeworkResult < ApplicationRecord
   has_one :homework_assignment
   has_attached_file :file, path: ':rails_root/public/homework_results/:filename', url: '/public/homework_results/:basename.:extension'
-  validates_attachment :file, :content_type => {:content_type => ["application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain"]}
+  validates_attachment :file, :content_type => {:content_type => ["application/pdf",
+                                                                  "application/msword",
+                                                                  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                                                                  "text/plain",
+                                                                  "audio/mpeg",
+                                                                  "audio/mpeg3",
+                                                                  "audio/wav",
+                                                                  "image/pjpeg",
+                                                                  "image/jpeg",
+                                                                  "application/zip"
+  ]
+  }
 end
