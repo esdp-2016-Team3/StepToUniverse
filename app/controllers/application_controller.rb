606eccba44af513488120a78c197fac8ccd206b6
Maxim <karamba92@mail.ru>
2016-11-08 21:17:28 +0600

    #75 при логине автоматом кидает теперь в ЛК

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path_for(resource)
+    if current_user.position_id==2
+      teacher_cabinet_path
+    else
+      student_cabinet_path(current_user)
+    end
+  end
+
   private
 
   def configure_permitted_parameters
