85744e8671f4031ebacabe2efb2f82918bede020
Maxim <karamba92@mail.ru>
2016-11-10 14:16:48 +0600

    Revert "Изменения связанные с сервером locum"
    This reverts commit 1faa6cf247e8f956a236a6c8d8387c7987848a5f.

--- config/deploy/locum.rb
+++
@@ -1,6 +0,0 @@
-role :app, %w(hosting_karambakg@chromium.locum.ru)
-role :web, %w(hosting_karambakg@chromium.locum.ru)
-role :db, %w(hosting_karambakg@chromium.locum.ru)
-
-set :ssh_options, forward_agent: true
-set :rails_env, :production
