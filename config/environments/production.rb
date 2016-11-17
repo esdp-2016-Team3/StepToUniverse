5a2f9039fe320f23bda2861a23717118495da26a
Maxim <karamba92@mail.ru>
2016-11-17 18:54:53 +0600

    #94 стопудова правильные настройки мейлера. Просто надо в самой почте разрешение включить

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            'socialhubs2016@gmail.com',
-      password:             'bishkek2016',
+      user_name:            'esdp20163@gmail.com',
+      password:             'esdp2016',
       authentication:       'plain',
       enable_starttls_auto: true  }
 
