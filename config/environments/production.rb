bbe12991df85eaa8c2f65c8ebf31a85e69fb775f
Maxim <karamba92@mail.ru>
2016-11-17 15:45:19 +0600

    #94 настройки для почты для продакшена

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,39 @@
 Rails.application.configure do
   # для отправки писем админу
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+        'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
+  config.assets.raise_runtime_errors = true
+
+  config.action_mailer.default_url_options = {:host => 'step2universe'}
+
+  # для mailcatcher
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  # config.action_mailer.raise_delivery_errors = false
+
+  config.force_ssl = false
+
   config.action_mailer.smtp_settings = {
       :address => 'smtp.gmail.com',
       :port => 587,
@@ -11,14 +45,28 @@ Rails.application.configure do
       :openssl_verify_mode => 'none'
   }
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
 
-  config.action_mailer.perform_caching = false
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-  config.assets.raise_runtime_errors = true
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
-  config.action_mailer.default_url_options = {:host => 'http://step2universe.com'}
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #______________________________________________#
 
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -77,7 +125,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "esdp-2016-3_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
