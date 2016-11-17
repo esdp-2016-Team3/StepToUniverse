56d767562be611311fc721a4b33d0008d66ee7db
Maxim <karamba92@mail.ru>
2016-11-17 17:39:24 +0600

    #94 настройки продакшена почты

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
 
   config.assets.raise_runtime_errors = true
