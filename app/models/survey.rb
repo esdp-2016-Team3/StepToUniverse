58ec4279dda4a47b25484575aa1556fcfe54b04f
Iliya Svetlichnii <batya2095@gmail.com>
2016-11-24 16:29:39 +0600

    #100 исправил расписание преподавателя, теперь у каждого препода свое расписание

--- app/models/survey.rb
+++
@@ -1,5 +0,0 @@
-class Survey < ApplicationRecord
-    belongs_to :homework
-	has_many :homework_questions, dependent: :destroy
-	accepts_nested_attributes_for :homework_questions, :reject_if => lambda { |a| a[:title].blank? }, allow_destroy: true
-end
