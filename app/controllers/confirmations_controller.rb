f61e9e6529f7fa03b0dc490581bcab95525a0bf4
Maxim <karamba92@mail.ru>
2016-11-28 20:57:02 +0600

    #113 всем админам отправляет уведомление о новом пользователе

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,10 +2,14 @@ class ConfirmationsController < Devise::ConfirmationsController
   protected
     def after_confirmation_path_for(resource_name, resource)
       if signed_in?(resource_name)
-        ToAdminMailer.reg_alert.deliver
+        AdminUser.all.each do |admin|
+         ToAdminMailer.reg_alert(admin).deliver_now
+        end
         signed_in_root_path(resource)
       else
-        ToAdminMailer.reg_alert.deliver
+        AdminUser.all.each do |admin|
+          ToAdminMailer.reg_alert(admin).deliver_now
+        end
         new_session_path(resource_name)
       end
     end
