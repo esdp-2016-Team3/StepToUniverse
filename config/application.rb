7c3a1b54d3f18ca10ab3e0248164eafec0122ce4
Maxim <karamba92@mail.ru>
2016-11-30 15:43:01 +0600

    113 добавил config.active_record.schema_format = :sql в апликатион.рб

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+config.active_record.schema_format = :sql
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
