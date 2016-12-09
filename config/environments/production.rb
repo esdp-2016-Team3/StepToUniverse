b92fc3c14326786c5e66812bd1499a3c0afc8920
Maxim <karamba92@mail.ru>
2016-12-09 22:43:44 +0600

    настройки скедитора для ассетов true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += Ckeditor.assets
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
