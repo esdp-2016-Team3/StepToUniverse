363935c692ac48592d7351923af99027951315d0
Iliya Svetlichnii <batya2095@gmail.com>
2016-12-09 18:11:39 +0600

    отрефакторил код тестов по определению уровня языка

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,8 @@ class PagesController < ApplicationController
   def sample_test
     @questions = []
     level = Level.all.count
+    
     count = 1
-
     level.times do 
       @question = Question.where(level_id: count, is_active: true).order("RANDOM()").limit(5)
       @question.each do |question|
@@ -23,48 +23,8 @@ class PagesController < ApplicationController
   end
 
   def result
-    arr = []
-    @answers = []
-
-    @level_1 = []
-    @level_2 = []
-    @level_3 = []
-    @level_4 = []
-
-    params.values.each do |value|
-      if value.to_i != 0
-        arr.push value.to_i
-      end
-    end
-
-    arr.each do |elem|
-      @answers.push Answer.find(elem)
-    end
-
-    @answers.each do |answer|
-      if answer.is_correct == true
-        if answer.question.level.id == 1
-          @level_1.push answer
-        elsif answer.question.level.id == 2
-          @level_2.push answer
-        elsif answer.question.level.id == 3
-          @level_3.push answer
-        elsif answer.question.level.id == 4
-          @level_4.push answer
-        end
-      end
-    end
-
-    beginner = Beginner.new('Beginner', @level_1.count)
-    elementary = Elementary.new('Elementary', @level_2.count)
-    intermediate = Intermediate.new('Intermediate', @level_3.count)
-    upper = UpperIntermediate.new('Upper-Intermediate', @level_4.count)
-
-    array_levels = [beginner, elementary, intermediate, upper]
-
     result_new = Result.new
-    @result = result_new.define(array_levels)
-  
+    @result = result_new.define(counting_result(params))
   end
   
   def literature_list
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,4 +1,46 @@
 module PagesHelper
+  include Levels
+  
+  def counting_result(params)
+    arr = []
+    @answers = []
+
+    @level_1 = []
+    @level_2 = []
+    @level_3 = []
+    @level_4 = []
+
+    params.values.each do |value|
+      if value.to_i != 0
+        arr.push value.to_i
+      end
+    end
+
+    arr.each do |elem|
+      @answers.push Answer.find(elem)
+    end
+
+    @answers.each do |answer|
+      if answer.is_correct == true
+        if answer.question.level.id == 1
+          @level_1.push answer
+        elsif answer.question.level.id == 2
+          @level_2.push answer
+        elsif answer.question.level.id == 3
+          @level_3.push answer
+        elsif answer.question.level.id == 4
+          @level_4.push answer
+        end
+      end
+    end
+
+    beginner = Beginner.new('Beginner', @level_1.count)
+    elementary = Elementary.new('Elementary', @level_2.count)
+    intermediate = Intermediate.new('Intermediate', @level_3.count)
+    upper = UpperIntermediate.new('Upper-Intermediate', @level_4.count)
+
+    array_levels = [beginner, elementary, intermediate, upper]
+  end
 
 	def homeworks_student(user_id)
 		homework_assignments = []
